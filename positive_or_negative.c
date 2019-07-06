21495f50f05aeeaf7900b9b0e3e928e5b466ce8d
Swarnaprabha M <swarnaprabhamaha@gmail.com>
2019-07-06 16:53:38 +0100

    Create positive_or_negative.c

---
+++ positive_or_negative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("number");
+scanf("%d",&n);
+if(n>0)
+{
+  printf("possive");
+}
+else if(n<0)
+{
+  printf("negative");
+}
+else
+{
+  printf("zero");
+}
+}
+
+
+
+/* -4
+  negative */
