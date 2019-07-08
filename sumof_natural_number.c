88e2e1df60e81918f0ca3fb6e5d8da6436d7a4c5
Swarnaprabha M <swarnaprabhamaha@gmail.com>
2019-07-08 17:54:39 +0530

    Create sumof_natural_number.c

---
+++ sumof_natural_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i=0;
+    int sum=0;
+    clrscr();
+    printf("\n");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+    sum=sum+i;
+    i++;
+    }
+     printf("%d",sum);
+
+    getch();
+   return 0;
+}
+
+
+
+
+/* 4
+   10    */
