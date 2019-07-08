129604def1e58bb2014656dcbf96033b164bd7a1
Swarnaprabha M <swarnaprabhamaha@gmail.com>
2019-07-08 17:43:39 +0530

    Create hello_n_times.c

---
+++ hello_n_times.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i;
+    clrscr();
+    printf("\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    printf("hello\n");
+    }
+    getch();
+   return 0;
+}
+
+/* hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello */
