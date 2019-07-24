9b61f7ae1222dfcba3cfafe8838ce0bf4cd5462e
Swarnaprabha M <swarnaprabhamaha@gmail.com>
2019-07-24 18:45:36 +0530

    Create ascii_valueof_charater.c

---
+++ ascii_valueof_charater.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf(" ");
+    char c;
+    printf("Enter a character : ");
+    scanf("%c" , &c);
+    printf("\n\nASCII value of %c = %d",c,c);
+    return 0;
+
+}
+
+/* Enter a character : A
+ASCII value of  A= 65 */
